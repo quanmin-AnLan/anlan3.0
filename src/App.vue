c76c31815a2d74986b5155ec6c03e1174b234fe3
anlan <anlanbupang@vip.qq.com>
2022-08-03 23:31:49 +0800

    add: 备案

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,13 @@
 <template>
   <router-view></router-view>
 </template>
+
+<style>
+body {
+    padding: 0;
+    margin: 0;
+}
+a {
+  text-decoration: none;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,31 @@
 <template>
-    <div>
-        --*开发中*--
-    </div>
+    <section class="main">
+        <div>
+            --*开发中*--
+        </div>
+        <footer class="common-footer">
+            <a href="https://beian.miit.gov.cn/" target="_blank" class="footer-link">京ICP备20019379号</a>
+        </footer>
+    </section>
 </template>
+
+<style lang="less" scoped>
+.main {
+    min-height: 100vh;
+    position: relative;
+}
+.common-footer {
+    position: absolute;
+    bottom: 0;
+    text-align: center;
+    width: 100%;
+    height: 36px;
+    line-height: 36px;
+}
+.footer-link {
+    color: #006eff;
+    &:hover {
+        color: #f00;
+    }
+}
+</style>
